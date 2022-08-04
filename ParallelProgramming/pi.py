0810d6ed3c521ec3feb8ef4f22ef2ea852f58f5a
Mohamed Elashri <muhammadelashri@gmail.com>
2022-08-04 16:17:56 -0400

    Add comments to explain `pi.py` script

--- ParallelProgramming/pi.py
+++ ParallelProgramming/pi.py
@@ -18,22 +18,38 @@ from multiprocessing import Pool, current_process
 import time
 
 
-def pi(p):
+def pi(p: int) -> Decimal:
+    """
+    Calculate pi to the specified number of digits using the
+    Bailey-Borwein-Plouffe formula.
+
+    :param p: The number of digits after the decimal point.
+    :return: The value of pi as a Decimal.
+    """
     getcontext().prec = p
-    # return Bailey-Borwein-Plouffe formula
     return sum(1/Decimal(16)**k * (Decimal(4)/(8*k+1) -
                                    Decimal(2)/(8*k+4) -
                                    Decimal(1)/(8*k+5) -
                                    Decimal(1)/(8*k+6)) for k in range(p))
 
 
-def main():
+def main() -> None:
+    """
+    This is a multi-line Google style docstring.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
 
     start = timer()
 
     with Pool(3) as pool:
         results = pool.map(pi, range(1, 10))
-        print(results)
+        #print(results)
+        print("Pi Estimation is: {}".format(results))
 
     end = timer()
     print("Time taken: ", end-start)
