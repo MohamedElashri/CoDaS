22472bd9ac15a01c29eb9866ae77b1c1a4d1de7a
Mohamed Elashri <muhammadelashri@gmail.com>
2022-08-04 16:04:13 -0400

    refactoring `pi_mc.py` script

--- ParallelProgramming/pi_mc.py
+++ ParallelProgramming/pi_mc.py
@@ -1,12 +1,19 @@
-from logging import raiseExceptions
-import random
 import math
 import multiprocessing
-from signal import raise_signal
+import random
 import time
+from logging import raiseExceptions
+from signal import raise_signal
+
 random.seed(42)
 
-def mc(n):
+
+def mc(n: int) -> int:
+    """
+    This function is used to calculate the value of pi
+    :param n: the number of points
+    :return: the value of pi
+    """
     cout = 0
     for i in range(n):
         x = random.random()
@@ -16,18 +23,18 @@ def mc(n):
     return cout
 
 
-def main():
+def main() -> None:
+    """
+    The main function to call multiprocessing routine.
+    """
     n_steps = int(1e7)
     n_cpu = multiprocessing.cpu_count()
-    
     print("Number of CPUs is: {}".format(n_cpu))
-    
-    n_processes = 4 # Assign number of processes to use
+    n_processes = 4  # Assign number of processes to use
     # if n_processes is 0, then it will use the default number of processes
     if n_processes == 0:
         n_processes = n_cpu
     print("Number of processes to use is: {}".format(n_processes))
-   
     start = time.time()
     pool = multiprocessing.Pool(n_processes)
     mp = pool.map(mc, [n_steps] * n_processes)
